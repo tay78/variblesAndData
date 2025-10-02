b5d63a01444eff80da3e89b0363505aa0bb17751
tay78 <tkwilliams-s@cps.edu>
2025-10-02 14:41:00 +0000

    oof  visitior age no work

--- operators.js
+++ operators.js
@@ -6,17 +6,28 @@
 // Add the two numbers and store the result in a new variable called sum.
 // Print the result using console.log()..
 
-
-
+let num1 = 12;
+let num2= 51;
+let sum;
+let difference;
+let product;
+let combinedString;
+let str1= "pancake ";
+let str2 = "hungry";
+sum=num1+num2;
+console.log(sum);
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
 // Print the result.
 
-
+difference=num1-num2;
+console.log(difference);
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
 // Print the result.
 
+product=num1*num2;
+console.log(product);
 
 
 // String Operations
@@ -26,3 +37,5 @@
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
 // Print the result.
 
+combinedString=str1+str2;
+console.log(combinedString);
